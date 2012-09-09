6611789bfc283de670c3319456659cbcefe12cd8
m.filippovich <m.filippovich@fun-box.ru>
2012-09-09 15:10:48 +0300

    setup project-specific namespace for sidekiq

--- templates/deploy/config/initializers/sidekiq.rb
+++ templates/deploy/config/initializers/sidekiq.rb
@@ -1,7 +1,11 @@
+require 'yaml'
+require 'sidekiq'
+require 'sidekiq-scheduler'
+
 if defined? SidekiqScheduler
-  require 'yaml'
-  require 'sidekiq'
-  require 'sidekiq-scheduler'
+  Sidekiq.configure_server do |config|
+    config.redis = { :url => 'redis://localhost:6379/0', :namespace => '<%= app_name %>' }
+  end
 
   Sidekiq.schedule = YAML.load(File.open("#{Rails.root}/config/sidekiq.yml"))["schedule"]
 end
