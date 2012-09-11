d468bf0b079f07167c7b723d802bf5f885dd3abc
m.filippovich <m.filippovich@fun-box.ru>
2012-09-12 00:05:55 +0300

    fix staging upstart

--- templates/deploy/config/deploy/staging.rb
+++ templates/deploy/config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :ssh_options, {:forward_agent => true, :keys => %w{~/.ssh/id_rsa}}
 
-set :user, "vagrant"
+role :app, "#{user}@5.9.90.15", primary: true
+role :web, "#{user}@5.9.90.15", primary: true
+role :db, "#{user}@5.9.90.15", primary: true
 
-role :app, "#{user}@178.63.98.150:2222", primary: true
-role :web, "#{user}@178.63.98.150:2222", primary: true
-role :db, "#{user}@178.63.98.150:2222", primary: true
+set :deploy_to, "/srv/staging/#{application}"
