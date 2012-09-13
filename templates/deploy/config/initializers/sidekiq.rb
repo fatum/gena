f3c1937a65892b7ada50f0994f051c92dfd266ac
m.filippovich <m.filippovich@fun-box.ru>
2012-09-14 00:41:19 +0300

    update deploy setup

--- templates/deploy/config/initializers/sidekiq.rb
+++ templates/deploy/config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
-if defined? SidekiqScheduler
-  require 'yaml'
-  require 'sidekiq'
-  require 'sidekiq-scheduler'
+require 'yaml'
+require 'sidekiq'
+require 'sidekiq-scheduler'
 
-  Sidekiq.configure_server do |config|
-    config.redis = { :url => 'redis://localhost:6379/0', :namespace => '<%= app_name %>' }
-  end
+Sidekiq.configure_server do |config|
+  config.redis = { :url => 'redis://localhost:6379/2', :namespace => 'applenews' }
+end
 
+if defined? Sidekiq.schedule
   Sidekiq.schedule = YAML.load(File.open("#{Rails.root}/config/sidekiq.yml"))["schedule"]
 end
