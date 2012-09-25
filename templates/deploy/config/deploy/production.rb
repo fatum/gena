8646ce6fc003d246d3b3e18b182fd9b7be246e5c
m.filippovich <m.filippovich@fun-box.ru>
2012-09-25 22:08:52 +0300

    add rails_env options and sphinx config

--- templates/deploy/config/deploy/production.rb
+++ templates/deploy/config/deploy/production.rb
@@ -1,4 +1,5 @@
 set :ssh_options, {:forward_agent => true, :keys => %w{~/.ssh/id_rsa}}
+set :rails_env, "production"
 
 role :app, "#{user}@5.9.90.15", primary: true
 role :web, "#{user}@5.9.90.15", primary: true
